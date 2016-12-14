077e6433bf87cf957481a39c92c7b48057fa55e4
Matt Hamlin <hamlim@outlook.com>
2016-12-14 18:33:08 -0500

    adding the base of the repo plus Button component

---
+++ src/button/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import cxs from 'cxs';
+import * as BaseStyles from '../styles';
+
+
+export default const Button = (clickHandler, filled, fill, color, ...props) => {
+  const buttonStyles = cxs({
+    all: 'unset',
+    display: 'inline-block',
+    textAlign: 'center',
+    height: '2em',
+    lineHeight: '2em',
+    padding: '.5em 1em',
+    backgroundColor: this.props.filled ? this.props.fill : BaseStyles.colors.white,
+    color: this.props.color,
+    border: 'solid 1px currentColor',
+    borderRadius: '4px',
+    transition: 'color 250ms ease, background-color 250ms ease'
+    ':hover': {
+      backgroundColor: !this.props.filled ? this.props.color : BaseStyles.colors.white,
+      color: !this.props.filled ? this.props.color : BaseStyles.colors.white,
+      transition: 'color 250ms ease, background-color 250ms ease'
+    },
+    ':focus': {
+      backgroundColor: !this.props.filled ? this.props.color : BaseStyles.colors.white,
+      color: !this.props.filled ? this.props.color : BaseStyles.colors.white,
+      transition: 'color 250ms ease, background-color 250ms ease'
+    }
+  });
+  return (
+    <button onClick={clickHandler()} className={buttonStyles} type="button">
+      {this.props.children}
+    </button>
+  );
+};
---
+++ src/styles.js
@@ -0,0 +1,15 @@
+export default styles = {
+  colors: {
+    white: '#fff',
+    black: '#000',
+    primary: '#35d642',
+    secondary: '#2992d8' 
+  },
+  spacing: {
+    one: '4px',
+    two: '8px',
+    three: '12px',
+    four: '16px',
+    five: '20px'
+  }
+};
